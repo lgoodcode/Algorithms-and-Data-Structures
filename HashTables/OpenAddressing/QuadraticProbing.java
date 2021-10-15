767ee6f9c4ca6a35b434800e7ac0408bba5d1e5d
Lawrence <lawrencegood55@gmail.com>
2021-10-14 21:11:49 -0700

    QuadatricProbing
    Created the QuadraticProbing hashtable

---
+++ HashTables/OpenAddressing/QuadraticProbing.java
@@ -0,0 +1,139 @@
+package Hashtables.OpenAddressing;
+
+import Hashtables.Entry;
+import Hashtables.AbstractHashtable;
+import Hashtables.exceptions.HashtableFullException;
+import static Hashtables.HashTableFunctions.isPrime;
+
+/**
+ * <h4>Tip: Using a large value, preferrably a prime number, for the table size
+ * is a must due to the nature of the hash function being quadratic, it will
+ * produce larger index values much more quickly. Doing so will prevent any
+ * unwanted errors from occurring.</h4>
+ * 
+ * Quadratic Probing uses a hash function of the form:
+ *
+ * <p>
+ * <pre>
+ * h(k, i) = (h^(k) + c1(i) + c2(i^2)) mod m
+ * </pre>
+ * </p>
+ * 
+ * <p>
+ * where h^ is the auxiliary hash function
+ * </p>
+ * 
+ * <p>
+ * The initial probe position is {@code h'(k)} later positions probed are offset
+ * by an arbitrary quadratic polynomial until an open slot is found.
+ * </p>
+ * 
+ * <p>
+ * If two keys have the same initial probe position, then their probe sequences
+ * are the same, since {@code h(k1, 0) = h(k2, 0)} implies
+ * {@code h(k1, i) = h(k2, i)}. This property leads to a milder form of
+ * clustering, known as "Secondary Clustering".
+ * </p>
+ *
+ * <p>
+ * As with linear probing, the initial probe determines the entire sequence, so
+ * only {@code m} distinct probe sequences are used.
+ * </p>
+ * 
+ * <p>
+ * Using a universal hash function, where the hash function is chosen randomly,
+ * independent of the keys, as the auxiliary hash function.
+ * </p>
+ */
+public final class QuadraticProbing<K, V> extends AbstractHashtable<K, V> {
+  private int a, b, p;
+
+  /**
+   * Initializes an empty, hashtable, with the specified size for the total
+   * capacity for the table.
+   * 
+   * <h4>Tip: Using a large value, preferrably a prime number, will prevent any
+   * unwanted errors from occurring.</h4>
+   * 
+   * @param size the specififed size of the hashtable maximum capacity
+   * 
+   * @throws IllegalArgumentException if the specified size is less than 1
+   */
+  public QuadraticProbing(int size, int prime) {
+    super(size);
+
+    if (!isPrime(prime))
+      throw new IllegalArgumentException("Illegal prime number given: " + prime);
+
+    p = prime;
+    a = (int) (Math.random() * p - 2) + 1;
+    b = (int) Math.random() * p - 1;
+  }
+
+  /**
+   * Returns the initialized hashtable number of elements it can hold.
+   * 
+   * @return the number of elements that can be stored.
+   */
+  public int capacity() {
+    return table.length;
+  }
+
+  /**
+   * 
+   * 
+   * @param k the key to hash
+   * @param i the number of rehashes
+   * @return the hashed index
+   */
+  protected int hash(K key, int i) {
+    int h1 = ((a * key.hashCode() + b) % p) % m;
+    return (int) (h1 + (0.5 * i) + (0.5 * Math.pow(i, 2))) % m;
+  }
+
+  /**
+   * Inserts an entry into the hashtable with the specified key and value.
+   * 
+   * @param key   the key of the entry
+   * @param value the value of the entry
+   * @throws IllegalArgumentException if the key or value is {@code null} or blank
+   * @throws HashtableFullException   if the attempting to insert while the table
+   *                                  is full
+   */
+  public synchronized boolean insert(K key, V value) throws HashtableFullException {
+    if (key == null || key.toString().isBlank())
+      throw new IllegalArgumentException("Key cannot be null or blank.");
+    if (value == null || value.toString().isBlank())
+      throw new IllegalArgumentException("Value cannot be null or blank.");
+    if (n == m)
+      throw new HashtableFullException(m);
+
+    for (int i=0, j = hash(key, i); i < m; i++, j = hash(key, i)) { 
+      if (table[j] == null) {
+        table[j] = new Entry<K, V>(key, value);
+        n++;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Internal method used by the other methods to lookup entries in the hashtable.
+   * 
+   * @param key the key to lookup
+   * @return the index of the element with the specified key or {@code -1} if not
+   *         found
+   * @throws IllegalArgumentException if the key or value is {@code null} or blank
+   */
+  protected synchronized int search(K key) {
+    for (int i=0, j = hash(key, i); i < m; i++, j = hash(key, i)) { 
+      if (table[j] != null && table[j].getKey().equals(key))
+        return j;
+    }
+
+    return -1; 
+  }
+
+}
---
+++ HashTables/__tests__/QuadraticProbing_Test.java
@@ -0,0 +1,202 @@
+package Hashtables.__tests__;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import Hashtables.OpenAddressing.QuadraticProbing;
+import Hashtables.exceptions.HashtableFullException;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class QuadraticProbing_Test {
+  QuadraticProbing<Integer, String> table;
+  QuadraticProbing<String, String> table2;
+  // Iterable<Integer> intKeys;
+  // Iterable<String> strKeys;
+
+  // @Test
+  // @Execution(ExecutionMode.CONCURRENT)
+  // void instantiating_overloaded_constructors() {
+  //   assertAll("instantiation for overloaded constructors", 
+  //     () -> assertDoesNotThrow(() -> new QuadraticProbing<>(), "default constructor"),
+  //     () -> assertDoesNotThrow(() -> new QuadraticProbing<>(3), "given a prime"),
+  //     () -> assertDoesNotThrow(() -> new QuadraticProbing<>(3, 1), "given a prime and size"),
+  //     () -> assertDoesNotThrow(() -> 
+  //       new QuadraticProbing<>(3, 1, 0.9f), "given a prime, size, and loadFactor"),
+  //     () -> assertDoesNotThrow(() -> new QuadraticProbing<>(3, 0.9f), "given prime and loadFactor"),
+  //     () -> assertDoesNotThrow(() -> new QuadraticProbing<>(0.9f, 1), "given loadFactor and size")
+  //   );
+  // }
+
+  // @ParameterizedTest(name = "{index}: prime = {0}, size = {1}, loadFactor = {2}")
+  // @Execution(ExecutionMode.CONCURRENT)
+  // @CsvSource({ "4, 1, 0.9", "3, 0, 0.9", "3, 1, 0.3" })
+  // void throws_when_instantiated_with_illegal_values(int prime, int size, float lf) {
+  //   assertThrows(IllegalArgumentException.class, () -> new QuadraticProbing<>(prime, size, lf));
+  // }
+
+  @Nested
+  class When_New {
+    int size = 10;
+    int prime = 1117;
+
+    @BeforeEach
+    void create_hashtable() {
+      table = new QuadraticProbing<>(size, prime);
+    }
+
+    @Test
+    void isEmpty() {
+      assertTrue(table.isEmpty());
+    }
+
+    @Test
+    void size_is_zero() {
+      assertEquals(0, table.size());
+    }
+
+    @Test
+    void capacity() {
+      assertEquals(size, table.capacity());
+    }
+
+    @Test
+    void toString_is_empty_object() {
+      assertEquals("{}", table.toString());
+    }
+
+    @ParameterizedTest(name = "key {index} => {0}")
+    @ValueSource(ints = {123, 2, 5})
+    void get_returns_null_for_any_key(int key) {
+      assertNull(table.get(key));
+    }
+
+    @ParameterizedTest(name = "key {index} => {0}")
+    @NullAndEmptySource
+    @ValueSource(strings = { " ", "  ", "\t", "\n" })
+    void throws_NullPointerException_for_null_and_empty_keys(String key) {
+      table2 = new QuadraticProbing<>(size, prime);
+
+      // Throws NullPointerException for null value and IllegalArgumentException
+      assertThrows(Exception.class, () -> table2.insert(key, "test"));
+    }
+  }
+
+  @Nested
+  @Tag("inserted")
+  class After_Inserting {
+
+    @BeforeEach
+    void insert_key_value() {
+      table = new QuadraticProbing<>(3, 1117);
+      assertDoesNotThrow(() -> table.insert(1, "one"));
+    }
+
+    @Test
+    void is_no_longer_empty() {
+      assertFalse(table.isEmpty());
+    }
+
+    @Test 
+    void size() {
+      assertEquals(1, table.size());
+    }
+
+    @Test
+    void has_get_and_delete_inserted_key() {
+      assertAll("has(), get(), and delete() key", 
+        () -> assertTrue(table.hasKey(1)),
+        () -> {
+          String val = table.get(1);
+          assertNotNull(val);
+          assertEquals("one", val);
+        },
+        () -> assertTrue(table.delete(1)),
+        () -> assertTrue(table.isEmpty())
+      );
+    }
+
+    @Test
+    void insert_throws_table_is_full() {
+      try {
+        table.insert(2, "two");
+        table.insert(3, "three");
+      } catch (HashtableFullException e) {}
+
+      assertThrows(HashtableFullException.class, () -> table.insert(4, "four"));
+    }
+
+    @Test
+    void to_String() {
+      assertEquals("{\n  \"Key: 1, value: one\"\n}", table.toString());
+    }
+  }
+
+  @Nested
+  class Many_Insertions {
+
+    @BeforeEach
+    void create_table() {
+      table = new QuadraticProbing<>(29, 1117);
+    }
+
+    @Test
+    void insertions() {
+      try {
+        assertTrue(table.insert(1, "one"));
+        assertTrue(table.insert(2, "two"));
+        assertTrue(table.insert(3, "three"));
+        assertTrue(table.insert(4, "four"));
+        assertTrue(table.insert(5, "five"));
+        assertTrue(table.insert(6, "six"));
+        assertTrue(table.insert(7, "seven"));
+        assertTrue(table.insert(8, "eight"));
+        assertTrue(table.insert(9, "nine"));
+        assertTrue(table.insert(10, "ten"));
+        assertTrue(table.insert(110, "eleven"));
+        assertTrue(table.insert(120, "twelve"));
+        assertTrue(table.insert(13, "thirteen"));
+        assertTrue(table.insert(14, "fourteen"));
+        assertTrue(table.insert(15, "fifteen"));
+        assertTrue(table.insert(16, "sixteen"));
+        assertTrue(table.insert(17, "seventeen"));
+        assertTrue(table.insert(18, "eighteen"));
+        assertTrue(table.insert(19, "nineteen"));
+        assertTrue(table.insert(20, "twenty"));
+      } catch (HashtableFullException e) {}
+
+      assertTrue(table.hasKey(1));
+      assertTrue(table.hasKey(2));
+      assertTrue(table.hasKey(3));
+      assertTrue(table.hasKey(4));
+      assertTrue(table.hasKey(5));
+      assertTrue(table.hasKey(6));
+      assertTrue(table.hasKey(7));
+      assertTrue(table.hasKey(8));
+      assertTrue(table.hasKey(9));
+      assertTrue(table.hasKey(10));
+      assertTrue(table.hasKey(110));
+      assertTrue(table.hasKey(120));
+      assertTrue(table.hasKey(13));
+      assertTrue(table.hasKey(14));
+      assertTrue(table.hasKey(15));
+      assertTrue(table.hasKey(16));
+      assertTrue(table.hasKey(17));
+      assertTrue(table.hasKey(18));
+      assertTrue(table.hasKey(19));
+      assertTrue(table.hasKey(20));
+    }
+
+  }
+
+}
